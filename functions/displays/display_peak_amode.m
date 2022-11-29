b56f3846b296e260eb42365dd3ec22915e16d231
Dennis Aprilla <dennis.ap.ch@gmail.com>
2022-11-29 23:51:00 +0100

    a-mode axes now can also show the peak. the peak representation is changed to a dot instead of an xline, so there is no confusion between timestamp on M-mode and peak on A-mode.

--- functions/displays/display_peak_amode.m
+++ functions/displays/display_peak_amode.m
@@ -1,6 +1,6 @@
 function display_peak_amode(current_axes, allpeaks, probeNumber_toShow, timestamp_toShow, tag)
 
-delete(findobj('Tag', tag));
+delete(findobj(current_axes, 'Tag', tag));
 
 % If the current_peaks variable is a cell, it means the user wants to run a full
 % ultrasound signal processing without any windowing. The peak could be
@@ -16,8 +16,14 @@ if (iscell(allpeaks.locations))
 % if the current_peaks is just one number, it means the user run an ultrasound 
 % signal processing with windowing.
 else
-    current_peaks = allpeaks.locations(probeNumber_toShow, timestamp_toShow);
-    xline(current_axes, current_peaks, '-', 'Color', 'b',  'Tag', tag);
+    % current_peaks = allpeaks.locations(probeNumber_toShow, timestamp_toShow);
+    % xline(current_axes, current_peaks, '-', 'Color', '#EDB120', 'LineWidth', 1.5, 'Tag', tag);
+
+    current_peak_locations = allpeaks.locations(probeNumber_toShow, timestamp_toShow);
+    current_peaks_sharpness = allpeaks.sharpness(probeNumber_toShow, timestamp_toShow);
+    hold(current_axes, 'on');
+    plot(current_axes, current_peak_locations, current_peaks_sharpness, 'or',  'MarkerFaceColor', 'y', 'MarkerSize', 10, 'LineWidth', 2, 'Tag', tag );
+    hold(current_axes, 'off');
 end
 
 end
